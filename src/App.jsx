cd1359dcb7eeef8c8e4377a7305415e5a08859e0
Jay patel <jayspatel190506@gmail.com>
2025-01-31 17:42:03 +0530

    some ui improvement

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import StylishForm from './components/stylishform'
 const url = `https://assihelp-backend.onrender.com/`;
+const text = "Files uploaded by you/your Friends 👇"
 function App() {
   const [dsa,setdsa] = useState(null);
   const [math,setmath] = useState(null);
@@ -26,21 +27,22 @@ function App() {
   return (
     
     <>
-       
     
     <div className="bg-slate-900 min-h-screen w-screen">
-      <div className='text-white text-4xl font-extrabold p-4 text-center'>Assignment Helper for CSE-Department</div>
+      <div className='text-white text-4xl font-extrabold p-4 text-center'><span className='text-blue-300'>Assignment Helper</span> for - 1st Year CSE F-Division</div>
+      <div className='text-center text-white font-bold text-2xl'>A Place Where you can Get/Contribute Assignments </div>
+      <div className='text-center text-white text-2xl font-bold mt-6 mb-2'>❤️ Your Contribution Will help Many Students...❤️</div>
       <div className='flex flex-wrap justify-center'>
         <div className='w-full md:w-1/2 lg:w-1/3 p-4'>
           <div className='bg-white h-full rounded-2xl p-4'>
             <div className='text-center text-2xl font-bold mb-4'>DSA - Assignments</div>
             <StylishForm name="dsa"/>
-            <h1 className='font-bold text-2xl mt-4'>Uploaded files Here 👇</h1>
+            <h1 className='font-bold text-2xl mt-4'>{text}</h1>
             <div>
               
                 <ul className='h-32 overflow-y-scroll'>
                 {dsa && dsa.map((val, idx) => (
-                    <li key={idx} className='text-blue-700 m-2 overflow-hidden p-2 text-center rounded-xl bg-slate-200 hover:bg-slate-300 font-medium underline'><a href={val.file} className='p-3'>{val.file}</a></li>
+                    <li key={idx} className='text-blue-700 m-2 overflow-hidden p-2 text-center rounded-xl bg-slate-200 hover:bg-slate-300 font-medium underline'><a href={val.file} className='p-3' target="_blank" rel="noopener noreferrer">{val.file}</a></li>
                   ))}
                 </ul>
                 
@@ -52,7 +54,7 @@ function App() {
           <div className='bg-white h-full rounded-2xl p-4'>
             <div className='text-center text-2xl font-bold mb-4'>Maths - Tutorial</div>
             <StylishForm name="maths"/>
-            <h1 className='font-bold text-2xl mt-4'>Uploaded files Here 👇</h1>
+            <h1 className='font-bold text-2xl mt-4'>{text}</h1>
             <div>
               
                 <ul className='h-32 overflow-y-scroll'>
@@ -69,12 +71,12 @@ function App() {
           <div className='bg-white h-full rounded-2xl p-4'>
             <div className='text-center text-2xl font-bold mb-4'>Web - Assignments</div>
             <StylishForm name="web"/>
-            <h1 className='font-bold text-2xl mt-4'>Uploaded files Here 👇</h1>
+            <h1 className='font-bold text-2xl mt-4'>{text}</h1>
             <div>
               
                 <ul className='h-32 overflow-y-scroll'>
                 {web && web.map((val, idx) => (
-                    <li key={idx} className='text-blue-700 m-2 overflow-hidden p-2 text-center rounded-xl bg-slate-200 hover:bg-slate-300 font-medium underline'><a href={val.file} className='p-3'>{val.file}</a></li>
+                    <li key={idx} className='text-blue-700 m-2 overflow-hidden p-2 text-center rounded-xl bg-slate-200 hover:bg-slate-300 font-medium underline'><a href={val.file} className='p-3' target="_blank" rel="noopener noreferrer">{val.file}</a></li>
                   ))}
                 </ul>
                 
@@ -86,12 +88,12 @@ function App() {
           <div className='bg-white h-full rounded-2xl p-4'>
             <div className='text-center text-2xl font-bold mb-4'>EEE Lab</div>
             <StylishForm name="eee"/>
-            <h1 className='font-bold text-2xl mt-4'>Uploaded files Here 👇</h1>
+            <h1 className='font-bold text-2xl mt-4'>{text}</h1>
             <div>
               
                 <ul className='h-32 overflow-y-scroll'>
                 {eee && eee.map((val, idx) => (
-                    <li key={idx} className='text-blue-700 m-2 overflow-hidden p-2 text-center rounded-xl bg-slate-200 hover:bg-slate-300 font-medium underline'><a href={val.file} className='p-3'>{val.file}</a></li>
+                    <li key={idx} className='text-blue-700 m-2 overflow-hidden p-2 text-center rounded-xl bg-slate-200 hover:bg-slate-300 font-medium underline'><a href={val.file} className='p-3' target="_blank" rel="noopener noreferrer">{val.file}</a></li>
                   ))}
                 </ul>
                 
@@ -103,12 +105,12 @@ function App() {
           <div className='bg-white h-full rounded-2xl p-4'>
             <div className='text-center text-2xl font-bold mb-4'>Electronics - Lab</div>
             <StylishForm name="electronic"/>
-            <h1 className='font-bold text-2xl mt-4'>Uploaded files Here 👇</h1>
+            <h1 className='font-bold text-2xl mt-4'>{text}</h1>
             <div>
               
                 <ul className='h-32 overflow-y-scroll'>
                 {elec && elec.map((val, idx) => (
-                    <li key={idx} className='text-blue-700 m-2 overflow-hidden p-2 text-center rounded-xl bg-slate-200 hover:bg-slate-300 font-medium underline'><a href={val.file} className='p-3'>{val.file}</a></li>
+                    <li key={idx} className='text-blue-700 m-2 overflow-hidden p-2 text-center rounded-xl bg-slate-200 hover:bg-slate-300 font-medium underline'><a href={val.file} className='p-3' target="_blank" rel="noopener noreferrer">{val.file}</a></li>
                   ))}
                 </ul>
                 
@@ -117,7 +119,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className='text-white text-center'>&copy; Copyrigth 2025 - AssignmentHelper.com</div>
+      <div className='text-white text-center'>&copy; Copyrigth 2025 - AssignmentHelper</div>
     </div>
   </>
     
--- src/components/stylishform.jsx
+++ src/components/stylishform.jsx
@@ -1,108 +1,108 @@
-import React,{useState,useEffect} from "react";
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const url = `https://assihelp-backend.onrender.com`;
 
 const StylishForm = (props) => {
-
-  
-
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false);
 
-const handleFileChange = (e) => {
-  console.log(e.target.files)
+  const handleFileChange = (e) => {
+    console.log(e.target.files);
     setFile(e.target.files[0]);
-};
-
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if(!file){
+      return;
+    }
     const formData = new FormData();
     formData.append("subject", props.name);
     formData.append("contribute", file);
-    setLoading(true)
+    setLoading(true);
     try {
-        const response = await fetch(url+'/user/upload', {
-            method: 'POST',
-            body: formData,
-        });
-        const data = await response.json();
-        console.log(data);
-        setFile(null);
-        e.target.reset();
-        toast.success('Your Assignment is Submitted! Please Refresh the page', {
-          position: "top-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-       
-          });
-          setLoading(false);
-        
+      const response = await fetch(url + "/user/upload", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+      setFile(null);
+      e.target.reset();
+      toast.success("Your Assignment is Submitted! Please Refresh the page", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      setLoading(false);
     } catch (error) {
-        console.error("Error:", error);
+      console.error("Error:", error);
     }
-};
+  };
 
   return (
-    
-      <form
-        method='post'
-        action={url+`/user/upload`}
-        encType='multipart/form-data'
-        onSubmit={(e)=>{
-          handleSubmit(e)
-        }}
-        className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg"
-      >
-        <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">Contribute Your Assignment</h2>
-        
-        <div className="mb-4">
-          <label
-            htmlFor={`${props.name}`}
-            className="block text-sm font-medium text-gray-600 mb-2 hover:cursor-pointer"
-          >
-            Note - * Please choose pdf file only
-          </label>
-          <input type="text" name="subject" defaultValue={`${props.name}`} className='hidden'/>
-          <input
-            name="contribute"
-            id={`${props.name}`}
-            type="file"
-            onChange={(e)=>{
-              handleFileChange(e)
-            }}
-            className="block w-full text-sm text-gray-600 file:border file:border-gray-300 file:py-2 file:px-4 file:rounded file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-400 hover:cursor-pointer"
-          />
-        </div>
-        
-        <button
-          type="submit"
-          className="w-full py-2 px-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 hover:cursor-pointer"
+    <form
+      method="post"
+      action={url + `/user/upload`}
+      encType="multipart/form-data"
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+      className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg"
+    >
+      <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">
+        Contribute Your Assignment
+      </h2>
 
+      <div className="mb-4">
+        <label
+          htmlFor={`${props.name}`}
+          className="block text-sm font-medium text-gray-600 mb-2 hover:cursor-pointer"
         >
-          {loading ? 'Submitting...' : 'Submit'}
-        </button>
-        <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick={true}
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
+          Note - * Please choose pdf file only
+        </label>
+        <input
+          type="text"
+          name="subject"
+          defaultValue={`${props.name}`}
+          className="hidden"
+        />
+        <input
+          name="contribute"
+          id={`${props.name}`}
+          type="file"
+          onChange={(e) => {
+            handleFileChange(e);
+          }}
+          className="block w-full text-sm text-gray-600 file:border file:border-gray-300 file:py-2 file:px-4 file:rounded file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-400 hover:cursor-pointer"
+        />
+      </div>
 
-/>
-      </form>
-   
+      <button
+        type="submit"
+        className="w-full py-2 px-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 hover:cursor-pointer"
+      >
+        {loading ? "Submitting..." : "Submit"}
+      </button>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </form>
   );
 };
 
